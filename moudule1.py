0562ba991e32281fed52d416c124d3331ed860a0
SinghRaghvendra <raghavsingh0027@gmail.com>
2025-05-14 14:50:56 +0530

    Updated from VSCODE

---
+++ moudule1.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def department(department):
+        self.department = department
+
+
+E1 = Employee("Raghav", 32, "M")
+E2 = Employee("Raghav2", 32, "M")
+
+print(E1.name)
+
+
+
